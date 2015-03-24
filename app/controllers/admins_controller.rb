33b074492bb61fabf635ca4b2e6fda7f7e8bbdea
navy <navyboys@gmail.com>
2015-03-23 18:13:30 -0700

    Assignment: Admin views payments - Part II

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,13 @@
+class AdminsController < ApplicationController
+  before_filter :require_user
+  before_filter :require_admin
+
+  private
+
+  def require_admin
+    if !current_user.admin?
+      flash[:error] = 'You are not authorized to do that.'
+      redirect_to home_path unless current_user.admin?  
+    end
+  end
+end
