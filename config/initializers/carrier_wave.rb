b79db01eb21a7635fb5287088c407b31a0b4d1d5
navy <navyboys@gmail.com>
2016-02-25 13:44:02 -0800

    use carrierwave-aws instead of fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,23 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    # config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+    config.storage = :aws
+    config.aws_bucket = 'navyflix'
+    config.aws_acl = 'public-read'
+    config.aws_credentials = {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'us-west-2'
     }
-    config.fog_directory  = 'navyflix'                     # required
+    # config.fog_provider = 'fog/aws'
+    # config.fog_credentials = {
+    #   provider:              'AWS',                        # required
+    #   aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
+    #   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+    # }
+    # config.fog_directory  = 'navyflix'                     # required
+  elsif Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,15 @@ require 'capybara/rails'
 require 'capybara/email/rspec'
 require 'sidekiq/testing'
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
 Capybara.server_port = 52662
 # Capybara.javascript_driver = :webkit
 
